a1874b865dfd46f8502839cbd66f6b393c8d0618
obi tochukwu <121336709+umbraobi@users.noreply.github.com>
2025-09-29 14:40:36 +0100

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://umbra-food-delivery-backend.onrender.com/'
+export const url = 'https://umbra-food-delivery-backend.onrender.com'
