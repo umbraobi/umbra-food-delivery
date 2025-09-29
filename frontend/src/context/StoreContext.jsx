1b2452c143c27ddde28c4a0d2b582e8db95205ce
obi tochukwu <121336709+umbraobi@users.noreply.github.com>
2025-09-29 14:38:54 +0100

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null);
 import axios from "axios";
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://umbra-food-delivery-backend.onrender.com/";
+  const url = "https://umbra-food-delivery-backend.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
