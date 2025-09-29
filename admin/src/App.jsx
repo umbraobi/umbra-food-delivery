cb47fc6d69ba703c7588e3a6b3027d0a61678d21
obi tochukwu <121336709+umbraobi@users.noreply.github.com>
2025-09-29 16:37:45 +0100

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "https://umbra-food-delivery-backend.onrender.com";
+  const url = 'https://umbra-food-delivery-backend.onrender.com';
   return (
     <div>
       <ToastContainer />
